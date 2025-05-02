d2d8014e5c4282659350a47103e07cba82380ebd
Taikakim <kim.ake@gmail.com>
2025-05-02 23:25:13 +0300

    Update run_gradio.py
    The new UI

--- run_gradio.py
+++ run_gradio.py
@@ -1,13 +1,12 @@
 from stable_audio_tools import get_pretrained_model
-from stable_audio_tools.interface.gradio import create_ui
+from stable_audio_tools.interface.gradio import create_ui_with_model_manager
 import json 
-
 import torch
 
 def main(args):
     torch.manual_seed(42)
 
-    interface = create_ui(
+    interface = create_ui_with_model_manager(
         model_config_path = args.model_config, 
         ckpt_path=args.ckpt_path, 
         pretrained_name=args.pretrained_name, 
@@ -20,6 +19,7 @@ def main(args):
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description='Run gradio interface')
+    # Same arguments as before
     parser.add_argument('--pretrained-name', type=str, help='Name of pretrained model', required=False)
     parser.add_argument('--model-config', type=str, help='Path to model config', required=False)
     parser.add_argument('--ckpt-path', type=str, help='Path to model checkpoint', required=False)
